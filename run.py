380869563fda3963cca441f5103a114a72daf3a5
yukithetupper <horatioesf@protonmail.com>
2025-07-17 16:54:53 +0100

    script structure

---
+++ run.py
@@ -0,0 +1,41 @@
+import pyopenms as oms
+import os
+
+def search_all_regions_in_file(filepath, searches):
+	exp = oms.MSExperiment()
+	oms.MzMLFile().load(filepath, exp)
+
+	print(filepath, "contains", exp.getNrSpectra(), "spectra.")
+
+	
+
+def main():
+
+	search_sheet_file = open("search_sheet.tsv", "r")
+	search_sheet = search_sheet_file.readlines()
+	search_sheet_file.close()
+
+	searches = []
+
+	for i in range(1, len(search_sheet)):
+		search_data = search_sheet[i].strip().split("\t")
+
+		search = {
+			"molecule_name": search_data[0],
+			"detector": search_data[1],
+			"precursor": search_data[2],
+			"mz_start": search_data[3],
+			"mz_end": search_data[4],
+			"rt_start": search_data[5],
+			"rt_end": search_data[6]
+		}
+
+		searches.append(search)
+
+	search_all_regions_in_file("data/" + os.listdir("data/")[0], searches)
+
+	#for filename in os.listdir("data/"):
+	#	search_all_regions_in_file("data/" + filename, searches)
+
+if __name__ == "__main__":
+	main()
