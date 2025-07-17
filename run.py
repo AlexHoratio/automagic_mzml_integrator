86d00d7285ed9f27fa66fec0c8573d666f493687
yukithetupper <horatioesf@protonmail.com>
2025-07-17 17:57:28 +0100

    ft peak integrator

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import pyopenms as oms
+import matplotlib.pyplot as plt
 import os
 
 def search_all_regions_in_file(filepath, searches):
@@ -31,17 +32,73 @@ def search_all_regions_in_file(filepath, searches):
 			case _:
 				print("Unknown MS Level:", spectrum.getMSLevel())
 
-	chromatograms = {}
 
-	for detector_id in demuxed_spectra.keys():
-		chromatograms[detector_id] = oms.MSChromatogram()
-		chromatograms[detector_id].set_peaks([
-				[spectrum.getRT() for spectrum in demuxed_spectra[detector_id]],
-				[sum(spectrum.get_intensity_array()) for spectrum in demuxed_spectra[detector_id]]
-			])
+	# We can now build and integrate chromatograms based on the searches we want to perform.
 
-	print(chromatograms)
+	integrator = oms.PeakIntegrator()
+	new_params = integrator.getParameters()
+	new_params.setValue("integration_type", "trapezoid")
+	integrator.setParameters(new_params)
 
+	for search in searches:
+		match search["detector"]:
+			case "FT":
+				chromatogram = oms.MSChromatogram()
+
+				mz_start = float(search["mz_start"])
+				mz_end = float(search["mz_end"])
+
+				rt_start = float(search["rt_start"])
+				rt_end = float(search["rt_end"])
+				
+				all_rt = [spectrum.getRT() for spectrum in demuxed_spectra["FT"]]
+				rt_interval = [rt for rt in all_rt if rt > rt_start and rt < rt_end]
+				#interval_idx = [all_rt.index(rt) for rt in rt_interval]
+				
+				#intensities_interval = [extract_summed_intensity_in_ion_range(spectrum, mz_start, mz_end) for spectrum in demuxed_spectra["FT"] if spectrum.getRT() > rt_start and spectrum.getRT() < rt_end]
+				intensities_interval = [extract_summed_intensity_in_ion_range(spectrum, mz_start, mz_end) for spectrum in demuxed_spectra["FT"]]
+
+				chromatogram.set_peaks([
+						all_rt,
+						intensities_interval
+					])
+
+				print(integrator.integratePeak(chromatogram, rt_start, rt_end).area)
+
+				#rts, ints = chromatogram.get_peaks()
+				#plt.plot(rts, ints)
+				#plt.show()
+
+			case "IT":
+				pass
+
+			case _:
+				print("Error in search_sheet.tsv! Invalid detector!")
+
+		print(search)
+
+	#chromatograms = {}
+
+	#for detector_id in demuxed_spectra.keys():
+	#	chromatograms[detector_id] = oms.MSChromatogram()
+	#	chromatograms[detector_id].set_peaks([
+	#			[spectrum.getRT() for spectrum in demuxed_spectra[detector_id]],
+	#			[sum(spectrum.get_intensity_array()) for spectrum in demuxed_spectra[detector_id]]
+	#		])
+
+def extract_summed_intensity_in_ion_range(spectrum, mz_start, mz_end):
+	intensity = 0
+
+	all_mz = spectrum.get_mz_array().tolist()
+	mz_interval = [mz for mz in all_mz if mz > mz_start and mz < mz_end]
+	interval_indexes = [all_mz.index(mz) for mz in mz_interval]
+
+	all_intensities = spectrum.get_intensity_array()
+	intensities_interval = [all_intensities[i] for i in interval_indexes]
+
+	intensity = float(sum(intensities_interval))
+
+	return intensity
 
 def main():
 
@@ -56,8 +113,7 @@ def main():
 
 		search = {
 			"molecule_name": search_data[0],
-			"detector": search_data[1],
-			"precursor": search_data[2],
+			"detector": search_data[1],			"precursor": search_data[2],
 			"mz_start": search_data[3],
 			"mz_end": search_data[4],
 			"rt_start": search_data[5],
